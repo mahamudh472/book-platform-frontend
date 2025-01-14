e35fc66207ad96833c232dc6a6ef5efe8c1bf9dd
mahamudh472 <mahadymahamudh472@gmail.com>
2025-01-14 15:19:48 +0600

    13 Jan update

--- admin.js
+++ admin.js
@@ -25,7 +25,7 @@ document.getElementById("menuToggle").addEventListener('click', () => {
     sidebarImg.classList.toggle('w-10')
     sidebarImg.classList.toggle('m-auto')
 
-    if(backText){
+    if (backText) {
         backText.classList.toggle('hidden')
     }
 
@@ -102,40 +102,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-// Get the context of the canvas
-const ctx = document.getElementById('myChart').getContext('2d');
 
-// Create the chart
-const myChart = new Chart(ctx, {
-    type: 'line', // Chart type: bar, line, pie, doughnut, etc.
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Augest', 'September', 'October', 'November', 'December'], // X-axis labels
-        datasets: [{
-            label: 'Views', // Chart legend
-            data: [10, 20, 5, 7, 9, 8, 0, 20, 19, 6, 12, 5], // Data points
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)'
-            ],
-            borderWidth: 1 // Border width for the bars
-        }]
-    },
-    options: {
-        responsive: true, // Makes the chart responsive
-        scales: {
-            y: {
-                beginAtZero: true // Starts y-axis at zero
-            }
-        }
-    }
-});
+
+//  copy text to clipboard
+function copy_to_clipboard(btn) {
+    const linkText = document.getElementById('book_link').innerText
+    navigator.clipboard.writeText(linkText)
+    btn.classList.toggle('fa-regular')
+    btn.classList.toggle('fa-copy')
+    btn.classList.toggle('fa')
+    btn.classList.toggle('fa-check')
+}
